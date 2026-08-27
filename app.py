b87f13d428bfcca909e42910eddd10eb968742eb
saito898 <saloaran1909@gmail.com>
2026-08-27 14:13:41 -0500

    Improve image processing and sidebar parameters
    Refactor image processing and translation parameters setup.

--- app.py
+++ app.py
@@ -80,36 +80,26 @@ if img_file_buffer is not None:
     bytes_data = img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
 
-    
     if filtro == 'Con Filtro':
-         cv2_img=cv2.bitwise_not(cv2_img)
+        cv2_img = cv2.bitwise_not(cv2_img)
     else:
-        cv2_img= cv2_img
-          
-        
+        cv2_img = cv2_img
+
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    text=pytesseract.image_to_string(img_rgb) 
-    st.write(text) 
+    text = pytesseract.image_to_string(img_rgb)
+    st.write(text)
 
-
-    st.markdown("""
-    <style>
-    [data-testid="stSidebar"] {
-    background-color: #0B1F3A;
-    }
-    </style>
-    """, unsafe_allow_html=True)
 
 with st.sidebar:
-        st.subheader("Parámetros de traducción")
-      
-      try:
-          os.mkdir("temp")
-      except:
-          pass
-      #st.title("Text to speech")
-      translator = Translator()
-      
+    st.subheader("Parámetros de traducción")
+
+    try:
+        os.mkdir("temp")
+    except:
+        pass
+
+    # st.title("Text to speech")
+    translator = Translator()
       #text = st.text_input("Enter text")
       in_lang = st.selectbox(
           "Seleccione el lenguaje de entrada de la imagen",
